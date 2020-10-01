62cbe4e0434744f15821798f64097aea69603ba8
wikugw <wikugw@gmail.com>
2020-10-01 08:06:43 +0700

    react reveal

--- src/components/MostPicked.js
+++ src/components/MostPicked.js
@@ -1,34 +1,39 @@
 import Button from 'elements/Button'
+import Fade from 'react-reveal/Fade';
 
 import React from 'react'
 
 export default function MostPicked(props) {
   return (
     <section className="container" ref={props.refMostPicked} >
-      <h4 className="mb-3">Most Picked</h4>
-      <div className="container-grid">
-        {props.data.map((item, index) => {
-          return (
-            <div key={index} className={`item column-4 ${index === 0 ? 'row-2' : 'row-1'}`}>
-              <div className="card card-featured">
-                <div className="tag">
-                  ${item.price}
-                  <span className="font-weight-light"> per {item.unit}</span>
+      <Fade bottom>
+        <h4 className="mb-3">Most Picked</h4>
+        <div className="container-grid">
+          {props.data.map((item, index) => {
+            return (
+              <Fade bottom delay={500 * index}>
+                <div key={index} className={`item column-4 ${index === 0 ? 'row-2' : 'row-1'}`}>
+                  <div className="card card-featured">
+                    <div className="tag">
+                      ${item.price}
+                      <span className="font-weight-light"> per {item.unit}</span>
+                    </div>
+                    <figure className="image-wrapper">
+                      <img className="image-cover" src={item.imageUrl} alt={item.name} />
+                    </figure>
+                    <div className="meta-wrapper">
+                      <Button type="link" href={`/property/${item._id}`} className="strecthed-link d-block text-white">
+                        <h5>{item.name}</h5>
+                      </Button>
+                      <span>{item.city}, {item.country}</span>
+                    </div>
+                  </div>
                 </div>
-                <figure className="image-wrapper">
-                  <img className="image-cover" src={item.imageUrl} alt={item.name} />
-                </figure>
-                <div className="meta-wrapper">
-                  <Button type="link" href={`/property/${item._id}`} className="strecthed-link d-block text-white">
-                    <h5>{item.name}</h5>
-                  </Button>
-                  <span>{item.city}, {item.country}</span>
-                </div>
-              </div>
-            </div>
-          )
-        })}
-      </div>
+              </Fade>
+            )
+          })}
+        </div>
+      </Fade>
     </section >
   )
 }
--- src/components/Testimoni.js
+++ src/components/Testimoni.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Fade from 'react-reveal/Fade';
 
 import Star from 'elements/Star'
 import Button from 'elements/Button'
@@ -7,30 +8,32 @@ import TestimonyFrame from '../assets/images/image-testimoni-frame.jpg'
 
 export default function Testimoni({ data }) {
   return (
-    <section className="container">
-      <div className="row align-items-center">
-        <div className="col-auto" style={{ marginRight: 70 }}>
-          <div className="testimonial-hero" style={{ margin: `30px 0 0 30px` }}>
-            <img src={data.imageUrl} alt="testimoni" className="position-absolute" style={{ zIndex: 1 }} />
-            <img src={TestimonyFrame} alt="testimoni frame" className="position-absolute" style={{ margin: `-30px 0 0 -30px`, zIndex: 0 }} />
+    <Fade bottom>
+      <section className="container">
+        <div className="row align-items-center">
+          <div className="col-auto" style={{ marginRight: 70 }}>
+            <div className="testimonial-hero" style={{ margin: `30px 0 0 30px` }}>
+              <img src={data.imageUrl} alt="testimoni" className="position-absolute" style={{ zIndex: 1 }} />
+              <img src={TestimonyFrame} alt="testimoni frame" className="position-absolute" style={{ margin: `-30px 0 0 -30px`, zIndex: 0 }} />
+            </div>
+          </div>
+          <div className="col">
+            <h4 style={{ marginBottom: 40 }}>
+              {data.name}
+            </h4>
+            <Star value={data.rate} height={35} width={35} spacing={4} />
+            <h5 className="h2 font-weight-light line-height-2 my-3">
+              {data.content}
+            </h5>
+            <span className="text-gray-500">
+              {data.familyName, data.familyOccupation}
+            </span>
+            <div>
+              <Button className="btn px-5" style={{ marginTop: 40 }} hasShadow isPrimary type="link" href={`/testimoni/${data._id}`}>Read Their Story</Button>
+            </div>
           </div>
         </div>
-        <div className="col">
-          <h4 style={{ marginBottom: 40 }}>
-            {data.name}
-          </h4>
-          <Star value={data.rate} height={35} width={35} spacing={4} />
-          <h5 className="h2 font-weight-light line-height-2 my-3">
-            {data.content}
-          </h5>
-          <span className="text-gray-500">
-            {data.familyName, data.familyOccupation}
-          </span>
-          <div>
-            <Button className="btn px-5" style={{ marginTop: 40 }} hasShadow isPrimary type="link" href={`/testimoni/${data._id}`}>Read Their Story</Button>
-          </div>
-        </div>
-      </div>
-    </section>
+      </section>
+    </Fade>
   )
 }
